45613af70382d9d4fb54693bfd2c4574770e2101
Devtecon <desarrollo@tecon.com.co>
2026-01-21 20:31:50 -0500

    delete project

--- src/constants/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    APP_NAME: process.env.REACT_APP_APP_NAME,
-
-    CLOUDINARY: {
-        CLOUD_NAME: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-        UPLOAD_PRESET: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
-    }
-};
--- src/styles/Home/homeStyles.js
+++
@@ -1,7 +0,0 @@
-// Dashboard JS styles solo para overrides dinámicos si es necesario
-export const dashboardStyles = {
-    topBar: {},
-    leftSection: {},
-    topActions: {},
-    title: {}
-};
